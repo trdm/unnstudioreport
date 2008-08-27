3599b4fe13388bf7bbd1f9ad8b787186e919250f
barilkovetal <barilkovetal@7726a62b-0e54-0410-8477-79dfdd70cdde>
2008-08-27 14:00:12 +0000

    Частично реализованная подложка

---
+++ branches/bary/Report/uoReport.h
@@ -0,0 +1,175 @@
+#ifndef UOREPORTCOMMON_H_INCLUDED
+#define UOREPORTCOMMON_H_INCLUDED
+
+/**************************************
+*
+*	(c) trdm, as Troshin D. V. 2008
+*  	GNU GPL3
+*  	unNStudio project
+*
+***************************************/
+
+namespace uoReport {
+
+//#include "uoReportDescr.h" // убрал, потому что доксиген дублирует описание в \subpage
+
+// На глазок вроде ничего..
+#define UORPT_SCALE_SIZE_DEF_VERTICAL 	15.0
+#define UORPT_SCALE_SIZE_DEF_HORIZONTAL 60.0
+
+#define uoReportVersion "0.1"
+#define rptSize qreal
+#define rptSizeNull 0.0
+
+/**
+	Размер крайней области объекта в которой курсор принимает
+	вид "перетаскивателя" и при клике в которой начинается
+	перетаскивание или изменение размера.
+*/
+#define UORPT_DRAG_AREA_SIZE 2.0
+
+#define rptRect QRectF
+
+/*
+	(примечание)
+	Scale - ячейка линейки,
+	Cell - ячейка основного поля, \n чисто для изучения английского :)
+*/
+
+
+
+struct uoLineSpan;
+struct uoRptNumLine;
+class uoSpanTree;
+class uoReportDoc;
+class uoReportCtrl;
+class uoReportView;
+class uoReportViewIteract;
+class uoReportDocBody;
+template <typename T> class uoNumVector;
+class uoReportLoader;
+class uoReportLoaderXML;
+class uoReportSelection;
+
+
+///\enum uoIntersectMode - варианты перечения отрезков
+typedef enum uoIntersectMode {
+	  ismNone 		= 0
+	, ismAbove 		= 1
+	, ismAboveIn 	= 3
+	, ismIn 		= 2
+	, ismInBelow 	= 6
+	, ismBelow 		= 4
+	, ismSurround 	= 7
+	, ismAboveUp 	= 9
+	, ismBelowDown 	= 12
+};
+typedef enum uoRptHeaderType {
+	  rhtUnknown = 0
+	, rhtVertical = 1
+	, rhtHorizontal = 2
+};
+
+typedef enum uoSearchDirection {
+	toUp,	///< направление поиска вверх
+	toDown	///< направление поиска ВНИЗ
+};
+
+typedef enum uoScaleSizePolicy
+{
+	uoSSP_fixed = 0 ///< расширения не происходит
+	, uoSSP_minimum ///< текущий размер минимально возможный, т.е. ячейка может расширяться.
+	, uoSSP_maximum ///< установленный размер является максимальным, т.е. ячейка будет стремиться сужаться.
+};
+
+///\enum uoRptStoreFormat - Формат сохранения отчета.
+typedef enum uoRptStoreFormat {
+	  uoRsf_Unknown = 0
+	, uoRsf_XML = 1
+	, uoRsf_Binary = 2
+	, uoRsf_HTML = 3
+};
+
+/**
+	\enum uoRptDocSparesType - типы запасных частей документа.
+	\brief - Перечисление пространственных частей документа/вьюва.
+	spares - запчасти ; запасные части; spare parts, spares.
+
+	Пока буду использовать для акселерации вычисления части документа по
+	пространственным координатам.
+	Vst = view Spares Type
+*/
+typedef enum uoRptSparesType {
+	  uoVst_Unknown = 0
+	, uoVst_GroupV = 1	///< рект вертикальной группы
+	, uoVst_GroupH		///< рект ГОРИЗОНтальной группы
+	, uoVst_ScaleVH 	///< Прямоугольник для выделения документа.
+	, uoVst_ScaleV 		///< Ячейка вертикальной линейки.
+	, uoVst_ScaleH 		///< Ячейка ГОРИЗОНтальной линейки.
+	, uoVst_SectionV 	///< Вертикальная секция.
+	, uoVst_SectionH 	///< Горизонтальная секция.
+	, uoVst_Cell	 	///< Ячейка таблицы или объединение.
+};	// Vst - (ViewSparesType)
+
+///\enum uoRptSelectionType типы выделения во вьюве.
+typedef enum uoRptSelectionType {
+	  uoRst_Unknown 	= 0
+	, uoRst_Document 	= 1	///< Выделен весь документ
+	, uoRst_Column			///< Выделена колонка
+	, uoRst_Columns			///< Выделены колонки
+	, uoRst_Row				///< Выделена строка
+	, uoRst_Rows			///< Выделены строки
+	, uoRst_Cell			///< Выделена ячейка
+	, uoRst_Cells			///< Выделены ячейки
+	, uoRst_Mixed			///< Миксированное выделение.
+};
+
+///\enum uoBorderLocType - расположения бордюра
+typedef enum uoBorderLocType
+{
+	uoBlt_Unknown = 0 // Отсчет по часовой.
+	, uoBlt_Top  = 1 ///< верхний бордер
+	, uoBlt_Right	 ///< правый бордер
+	, uoBlt_Bottom	 ///< нижний бордер
+	, uoBlt_Left  	 ///< левый бордер
+};
+
+///\enum uoReportUseMode - режим работы с отчетом разработка или использование
+enum uoReportUseMode {
+	/**
+		Режим разработки.
+		По умолчанию включает видимости секций, линейки, сетки.
+		Все остальное отключено. Не испускаетют сигналы "приКликеНаЯчейке"
+	*/
+	rmDevelMode = 0
+	/**
+		Режим отображения.
+		По умолчанию включает видимости группировок. Все остальное отключено.
+		Сигналы "приКликеНаЯчейке" испускаются со значением расшифровки.
+	*/
+	, rmUsingMode = 1
+};
+
+///\enum uoReportStateMode - режим взаимодействия с пользователем
+/// Например: редактирование ячейки, выделение групп ячеек, и т.п.
+enum uoReportStateMode {
+	rmsNone = 0 			///< Обычный режим. Серфинг с пом. КПК по вьюву.
+	, rmsResizeRule_Top  	///< Изменение размера вертикальной ячейки линейки.
+	, rmsResizeRule_Left 	///< Изменение размера ГОРИЗОНТАЛЬНОЙ ячейки линейки.
+	, rmsSelectionRule_Top  ///< Групповое выделения ячеек вертикальной линейки.
+	, rmsSelectionRule_Left ///< Групповое выделения ячеек ГОРИЗОНТАЛЬНОЙ линейки.
+};
+
+
+/// Запуск тестов для элементов отчета...
+extern void uoRunTest();
+
+class uoReportTest {
+	uoReportTest();
+	~uoReportTest();
+};
+
+} // namespace uoReport
+
+
+#endif // UOREPORTCOMMON_H_INCLUDED
---
+++ branches/bary/Report/uoReportDocBody.h
@@ -0,0 +1,88 @@
+/**************************************
+*
+*	(c) trdm, as Troshin D. V. 2008
+*  	GNU GPL3
+*  	unNStudio project
+*
+***************************************/
+
+#ifndef UOREPORTDOCBODY_H
+#define UOREPORTDOCBODY_H
+
+#include <QtCore>
+#include <QLinkedList>
+#include "uoReport.h"
+#include "uoNumVector.h"
+#include "uoReportLoader.h"
+
+
+
+namespace uoReport {
+
+///\struct uoRptNumLine - нумерованная "линия" отчета (строка или столбец)
+struct uoRptNumLine {
+	public:
+
+		uoRptNumLine(int ln)
+			: _line(ln), _size(-1), _hiden(false), _sellected(false)
+			{}
+
+	void 	setNumber(int nm) {_line = nm;}
+	int  	number() {return _line;}
+
+	void 	setSize(rptSize sz, bool isDef = false) {
+		if (isDef)	_sizeDef = sz;
+		else 		_size = sz;
+	}
+	rptSize size(bool isDef = false) {return isDef?_sizeDef:_size;}
+
+	bool 	hiden() {return _hiden;}
+	void 	setHiden(bool hd) {_hiden = hd;}
+
+	bool 	selected() {return _sellected;}
+	void 	setSelected(bool sl) {_sellected = sl;}
+
+	private:
+		int _line;
+		/**
+			некоторые пояснения. есть 2 размера: _size и _sizeDef
+			_sizeDef - размер ячейки, как он задан в процессе конфигурирования отчета. например как дефолтная высота.
+			_size    - размер ячейки, который она примет когда будет заполнена данными на этапе вывода.
+			так называемый "динамический" размер. Естественно для пространственных расчетов принимается _size,
+			но когда ячейка пуста, размер возвращается из _sizeDef.
+		*/
+		rptSize _size, _sizeDef;///< Дефолтный размер, устанновленный в конструкторе, к которому будет возвращаться размер.
+		bool _hiden, _sellected;
+};
+
+// по typename:
+// http://alenacpp.blogspot.com/2006/08/typename.html
+
+///\class uoHeaderScale Хранение и обработки длинных нумерованных структур. \n
+///\brief Предназначен для хранения и обработки длинных нумерованных структур. \n
+///
+/// Перечень структр: Заголовки колонок и строк табличного документа, самих \n
+/// строк документа, содержащих данные рядов ячеек. Данные ячеек - текст, его \n
+/// форматирование, рамки, данные по их объединению и т.п.
+/// (примечание) Scale - ячейка линейки, Cell - ячейка основного поля, \n чисто для изучения английского :)
+
+class uoHeaderScale
+	: public uoNumVector<uoRptNumLine>
+{
+	public:
+		uoHeaderScale();
+		~uoHeaderScale();
+
+	void printToDebug();
+	bool getHide(int nom );
+	void setHide(int nom, bool hide);
+	bool onStoreItems(uoReportLoader* loader);
+	void test();
+
+};
+
+
+
+} // namespace uoReport
+
+#endif // UOREPORTDOCBODY_H
