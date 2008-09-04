890cfa00a6015819d212f28340ab990e24f97b41
trdmval <trdmval@7726a62b-0e54-0410-8477-79dfdd70cdde>
2008-09-04 21:11:52 +0000

    lr-130 - баг поиска ячейки по координатам. + реализовал групповое выделение ячеек способом захвати и тяни. пока вертикальная линейка и без учета масштаба. масштаб глючит :)

--- Report/uoReportSelection.h
+++ Report/uoReportSelection.h
@@ -34,6 +34,9 @@ class uoReportSelection : public QObject
 		bool isShiftPress();
 
 		void startRowSelected(int nmRow);
+		void midleRowSelected(int nmRow);
+		void endRowSelected(int nmRow);
+
 		void startColSelected(int nmCol);
 		void startCellSelected(int nmCol, int nmRow);
 		void selectDocument();
@@ -43,7 +46,17 @@ class uoReportSelection : public QObject
 		uoRptSelectionType getSelectionType();
 
 	private:
+		/*
+			зачем нужны временные выделенные строки?
+			временные выделенные строки возникают когда пользователь откнопал
+			с нажатой клавишей контрол несколько строк, а теперь хочет выделить
+			интервал. и он нажимает на ячейку линейки и тащит мышку не отпуская.
+			в это время он может перекрыть уже выделенные строки, но потом вернуться
+			обратно, покинув их. выделение должно сохраниться.
+			поэтому я работаю не с _selRows а с _selRowsTmp.
+		*/
 		QList<int>* 	_selRows; 		///< Список выделенных строк
+		QList<int>* 	_selRowsTmp; 	///< Список временных выделенных строк
 		QList<int>* 	_selCols;		///< Список выделенных колонок
 		QList<QRect*>* _selSpans;		///< Список выделенных областей/спанов
 		QList<QRect*>* _selSpansCache;	///< Кешь выделенных областей/спанов
